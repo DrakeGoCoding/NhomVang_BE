a3ae577e1c3c0be9c4f6313e6204e1c136e667ef
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-11-04 16:49:24 +0700

    init morgan logger

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const cors = require('cors');
 app.use(cors());
 
+const rfs = require('rotating-file-stream');
+const path = require('path');
+const accessLogStream = rfs.createStream('access.log', {
+	interval: '1d',
+	path: path.join(__dirname, 'loggers')
+});
 const morgan = require('morgan');
 morgan.token('body', (req) => JSON.stringify(req.body));
-app.use(morgan(':date[iso] :method :url :body :status :res[content-length] - :response-time ms'))
+app.use(morgan('combined', { stream: accessLogStream }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_CONNECTION, (error) => {
