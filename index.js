c17433010dda2508b3650cbb42e460e4452358bd
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-11-08 22:38:03 +0700

    skip ci/cd env

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
+dotenv.config();
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-const cors = require('cors');
 app.use(cors());
 
-const rfs = require('rotating-file-stream');
-const path = require('path');
 const accessLogStream = rfs.createStream('access.log', {
 	interval: '1d',
 	path: path.join(__dirname, 'loggers')
 });
-const morgan = require('morgan');
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan('combined', { stream: accessLogStream }));
 
-const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_CONNECTION, (error) => {
 	if (error) {
 		throw new Error("Database connection failed.");
