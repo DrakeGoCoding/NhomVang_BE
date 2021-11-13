01078dbbdea1904b08f9e5ff828e2aa5f57fd1a3
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-11-13 17:12:06 +0700

    init cloudinary setup

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,50 +1,54 @@
-const { cloudinary } = require('../configs');
+const { cloudinary } = require('@configs');
+
+const CLOUDINARY_FOLDER = process.env.CLOUDINARY_FOLDER;
 
 /**
  * Upload an image to a folder
- * @param {String} fileStr64 file string base64
- * @param {String} folder folder name
+ * @param {String} file] file name or base64 string
+ * @param {String} folder folder path
  * @returns {String} public image url
  * @DrakeGoCoding 11/13/2021
  */
-const uploadImage = async (fileStr64, folder) => {
-	try {
-		const result = await cloudinary.uploader.upload(fileStr64, { folder });
-		return result.url;
-	} catch (error) {
-		next(error);
-	}
+const uploadImage = async (file, folder) => {
+	const result = await cloudinary.uploader.upload(
+		file,
+		{ folder: `/${CLOUDINARY_FOLDER}/${folder}` }
+	);
+	return result.url;
 }
 
 /**
- * Delete an image from a folder
- * @param {String} file file name
- * @param {String} folder folder name
+ * Delete an image by public_id
+ * @param {String} publicId
  * @DrakeGoCoding 11/13/2021
  */
-const deleteImage = async (file, folder) => {
-	try {
-		await cloudinary.uploader.destroy(`${folder}/${file}`);
-	} catch (error) {
-		next(error);
-	}
+const deleteImage = async (publicId) => {
+	await cloudinary.uploader.destroy(publicId);
+}
+
+/**
+ * Create a new folder
+ * @param {String} folder folder path
+ * @DrakeGoCoding 11/13/2021
+ */
+const createFolder = async (folder) => {
+	await cloudinary.api.create_folder(`${CLOUDINARY_FOLDER}/${folder}`);
 }
 
 /**
  * Delete a folder and its content
- * @param {String} folder folder name 
+ * @param {String} folder folder path
  * @DrakeGoCoding 11/13/2021
  */
 const deleteFolder = async (folder) => {
-	try {
-		await cloudinary.api.delete_folder(folder);
-	} catch (error) {
-		next(error);
-	}
+	const path = `${CLOUDINARY_FOLDER}/${folder}/`;
+	await cloudinary.api.delete_resources_by_prefix(path);
+	await cloudinary.api.delete_folder(path);
 }
 
 module.exports = {
 	uploadImage,
 	deleteImage,
+	createFolder,
 	deleteFolder
 };
