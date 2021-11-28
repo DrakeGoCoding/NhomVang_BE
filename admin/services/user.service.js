e9c8b0e38e528ec0391fbda2fc56eaa97e436e0f
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-11-28 20:22:07 +0700

    getAllNews limit offset

--- admin/services/user.service.js
+++ admin/services/user.service.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcrypt');
-// const User = require('@models/user');
-const User = require('../../models/user')
+const User = require('@models/user');
 const { responseUser } = require('@utils/responsor');
 const AppError = require('@utils/appError');
 const { NOT_FOUND_USER, FOUND_USER } = require('@constants/error');
@@ -32,7 +31,7 @@ const getAllUsers = async (filter = {}, limit = 20, offset = 0) => {
 				userList: [],
 				total: 0
 			}
-		}
+		};
 	}
 
 	return {
