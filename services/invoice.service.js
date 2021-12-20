90ed552ea553bd43a9f55e75657180117debc44c
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-12-21 00:36:43 +0700

    create invoice bugfix

--- services/invoice.service.js
+++ services/invoice.service.js
@@ -71,8 +71,8 @@ const createInvoice = async (userId, products) => {
         throw new AppError(400, "fail", NOT_FOUND_PRODUCT_IN_CART);
     }
 
-    cart.items = cart.items.filter(item => toRemoveFromCart.includes(item._id));
-    products = products.filter(item => toRemoveFromCart.includes(item._id.toString()));
+    cart.items = cart.items.filter(item => !toRemoveFromCart.includes(item._id));
+    products = products.filter(item => !toRemoveFromCart.includes(item._id.toString()));
 
     const newInvoice = await Invoice.create({
         user: userId,
